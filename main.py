e2a8114c576a3b74b2a49deaddd0a7a85e4866af
Shane <>
2020-12-04 14:24:28 -0500

    🐞✔Fixed Bug where it returns stored password from another user. Before, if the user wanted to view their password for a website, and the first user in the database (lets call him "Will") also had that same website credentials saved, it would return the website credentials for Will instead. Big security threat fixed

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from Crypto.Cipher import AES
 import bcrypt
 import mysql.connector
 from os import path
-from Crypto.Util.Padding import pad # for encrypting
+from Crypto.Util.Padding import pad  # for encrypting
 from Crypto.Util.Padding import unpad
 import random
-
 
 file_pwd = open("pwd.txt", "r")
 # "pwd.txt" has plain text password for the database.
@@ -26,11 +25,11 @@ mycursor = mydb.cursor()
 
 def create_account():
     username = input("""Username
->""")
+    >""")
     if 3 < len(username) < 15:
         password = input("""Password
->""")
-        if len(password) == 17:
+    >""")
+        if 20 > len(password) > 5:
             password = password.encode('utf-8')
             hashed = bcrypt.hashpw(password, bcrypt.gensalt(14))
             hashed = hashed.decode()
@@ -51,34 +50,38 @@ VALUES ("{new_max_id}", "{username}", "{hashed}");""")
 
             mydb.commit()
 
-        else:
+        elif len(password) <= 5:
+            print("Password is too short")
+            create_account()
+        elif len(password) >= 20:
+            print("Password is too large")
             create_account()
     else:
+        print("Invalid username length. Try again")
         create_account()
 
 
 def store_password():
-
     username = input("""Username
->""")
+    >""")
     mycursor.execute(f"""SELECT * FROM accounts WHERE username = '{username}' """)
     myresult = mycursor.fetchone()
     hashed_password = myresult[2].encode('utf-8')
     password = input("""Password
->""")
+    >""")
     if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
         account_id = myresult[0]
         location = input("""Website name
->""")
+    >""")
         sub_username = input("""What is your username for that website?
->""")
+    >""")
         the_password = input("""What is your password for that website?
-        >""")
+    >""")
         notes = input("""Any personal notes (DO NOT include confidential information) """)
 
         salt = random.randint(9999, 99999)
         salt = "salt" + str(salt)
-        pwd_salt = the_password+salt
+        pwd_salt = the_password + salt
         if account_id != "" and location != "" and sub_username != "" and the_password != "":
             mycursor.execute(f""" 
 INSERT INTO stored_passwords (account_id, location, notes, the_password, username, salt, website_username) 
@@ -101,11 +104,9 @@ def read_password():
         account_id = myresult[0]
         location = input("""Website name
     >""")
-        sub_username = input("""What is your username for that website?
-    >""")
         mycursor.execute(f"""
 SELECT *, REPLACE(CAST(AES_DECRYPT(the_password,'{password}') as char(10000)), salt, ""), salt
-FROM stored_passwords WHERE location = '{location}';
+FROM stored_passwords WHERE username = '{username}' AND location = '{location}';
 """)
         myresult = mycursor.fetchone()
         print(f"""
@@ -118,6 +119,4 @@ Notes: {myresult[3]}
         print("Failure")
 
 
-read_password()
-
-
+create_account()
