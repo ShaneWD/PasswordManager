6a1db66e67c8c9300781f312ab4be961a66da657
Shane <>
2020-12-22 12:09:15 -0500

    MAJOR BUGS; testing store_password feature

--- SQLite_main.py
+++ SQLite_main.py
@@ -2,6 +2,7 @@ import sqlite3
 import random
 import string
 import bcrypt
+from AES_encryption import encrypt as aes_encrypt, decrypt as aes_decrypt
 
 
 mydb = sqlite3.connect('SQLite_PWD.db')
@@ -14,7 +15,18 @@ mycursor.execute("""CREATE TABLE accounts (
         password TEXT NOT NULL
                 )""")
 '''
-
+'''
+mycursor.execute("""CREATE TABLE stored_passwords (
+        account_id INT NOT NULL UNIQUE REFERENCES accounts(account_id),
+        username TEXT NOT NULL,
+        location TEXT NOT NULL,
+        website_username TEXT NOT NULL,
+        the_password TEXT NOT NULL,
+        salt TEXT NOT NULL,
+        notes TEXT NULL
+                )""")
+'''
+# account_id, location, notes, the_password, username, salt, website_username
 # mycursor.execute("INSERT INTO accounts VALUES ('1', 'delete', 'delete' )")
 # mycursor.execute("SELECT * FROM ACCOUNTS")
 # data = mycursor.fetchall()
@@ -24,7 +36,7 @@ mycursor.execute("""CREATE TABLE accounts (
 
 def create_account():
     username = input("""Username
-    >""")
+    >""").lower()
     if 3 < len(username) < 15:
         mycursor.execute(f"""SELECT username FROM accounts WHERE username = '{username}' """)
         myresult = mycursor.fetchone()
@@ -75,3 +87,71 @@ def create_account():
         print("Invalid username length. Try again")
 
 
+def store_password():
+    username = input("""Username
+    >""")
+    print(username)
+    mycursor.execute(f"""SELECT * FROM accounts WHERE username = '{username}' """)
+    myresult = mycursor.fetchone()
+    print(myresult)
+    if not myresult:
+        return print("Failure")
+    hashed = myresult[2]
+    salt = myresult[3]
+    hashed_password = hashed.encode('utf-8')
+    password = input("""Password
+    >""")
+    password = password + salt
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        account_id = myresult[0]
+        location = input("""Website name
+    >""")
+        mycursor.execute(f"""
+SELECT username FROM stored_passwords WHERE location = '{location}' 
+and username = '{username}'""")
+        myresult = mycursor.fetchone()
+        # in order to see if that username already exists in the database
+        if not myresult:
+            sub_username = input("""What is your username for that website?
+        >""")
+            the_password = input("""What is your password for that website?
+        >""")
+            notes = input("""Any personal notes (DO NOT include confidential information) """)
+
+            def get_random_string(length):
+                # Random string with the combination of lower and upper case
+                letters = string.ascii_letters
+                result_str = ''.join(random.choice(letters) for i in range(length))
+                return result_str
+            salt_letter = get_random_string(8)
+            salt_number = random.randint(9999, 99999)
+            salt = "salt" + salt_letter + str(salt_number)
+            pwd_salt = the_password + salt
+            encrypted = aes_encrypt(password.encode(), pwd_salt)
+            # To add a random set of characters at the end of the plain-text password before encryption.
+            # This is because two identical passwords will result in the same encrypted text. Thus, salt is needed.
+            # For decryption,the program looks at the row salt and removes those characters after decryption.
+            '''
+            ex: 
+            Password = pwd123
+            A salt is generated and added to the password, as well as its own separate column. 
+            Before encryption, it looks is updated to (similar to): pwd123saltHnaH18723
+            Then it is encrypted 
+            After decryption, it looks like: pwd123saltHnaH18723
+            The program looks at the dedicated salt column, and removes it from the plain text after decryption. 
+            '''
+            if account_id != "" and location != "" and sub_username != "" and the_password != "":
+                mycursor.execute(f""" 
+    INSERT INTO stored_passwords (account_id, location, notes, the_password, username, salt, website_username) 
+    VALUES ("{account_id}", "{location}", "{notes}", {encrypted}, "{username}", "{salt}",
+    "{sub_username}");""")
+                mydb.commit()
+            else:
+                print("failure")
+        else:
+            print("That website name already exists")
+    else:
+        print("failure")
+
+
+store_password()
